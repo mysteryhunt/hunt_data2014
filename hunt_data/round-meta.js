254c2e898297ad9aeb3725e1cfbc21c79e71d7bf
James Clark <sbj@dimins.com>
2013-12-08 18:42:47 -0500

    Set up 'submit answer' popups for mit, events, and the wl rounds

---
+++ hunt_data/round-meta.js
@@ -0,0 +1,13 @@
+mh.submit_answer = function() {
+    var outer = $('#mh-submit-answer');
+    var inner = $('#mh-submit-answer-inner');
+    outer.stop(true, true);
+    if (outer.is(':visible')) {
+        outer.fadeToggle('slow', function() {
+            inner.html('');
+        });
+    } else {
+        inner.html('<iframe src="{{ root }}/dynamic/submit/metapuzzle/{{ round.round.url }}" id="mh-submit-answer-iframe"></iframe>');
+        outer.fadeToggle('slow');
+    }
+};
