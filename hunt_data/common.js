f651c8fb67a13a84ff9e2c39d041259ef76e1344
James Clark <sbj@dimins.com>
2013-12-12 15:23:42 -0500

    grey background behind submit answer popup, clicking that closes the popup.  white queen puzzle links should say 'submit answer' not 'submit title'

--- hunt_data/common.js
+++ hunt_data/common.js
@@ -1,14 +1,22 @@
 var mh = {};
-(function() {
-    $('#mh-controls > *').css('opacity',0).css('top',20).animate({
-	opacity: 1,
-	top: 0
-    }, 1000).mouseenter(function(e) {
-	$(e.target).animate({ top: -2 }, 100, 'linear').animate({ top: 0 }, 100, 'linear');
-    });
-    mh.plain_toggle = function() {
-	$(document.body).toggleClass('mh-plain mh-fancy');
-	var b = $('#mh-plain-toggle');
-	b.text(b.text() == 'Back' ? 'Printable' : 'Back');
+(function($){
+    $.fn.disableSelection = function() {
+        return this
+                 .attr('unselectable', 'on')
+                 .css('user-select', 'none')
+                 .css('user-drag', 'none')
+                 .on('selectstart', false)
     };
-})();
+})(jQuery);
+$('#mh-controls > *').css('opacity',0).css('top',20).animate({
+    opacity: 1,
+    top: 0
+}, 1000).mouseenter(function(e) {
+    $(e.target).animate({ top: -2 }, 100, 'linear').animate({ top: 0 }, 100, 'linear');
+});
+mh.plain_toggle = function() {
+    $(document.body).toggleClass('mh-plain mh-fancy');
+    var b = $('#mh-plain-toggle');
+    b.text(b.text() == 'Back' ? 'Printable' : 'Back');
+};
+$('#mh-controls, #mh-controls > *').disableSelection();
--- hunt_data/round-meta.js
+++ hunt_data/round-meta.js
@@ -1,13 +1,23 @@
 mh.submit_answer = function() {
+    var page = $('#mh-page');
     var outer = $('#mh-submit-answer');
+    outer.css('top', page.offset().top + 20);
+    outer.css('left', page.offset().left + page.innerWidth() - 20 - outer.outerWidth());
+    var bg = $('#mh-submit-answer-bg');
     var inner = $('#mh-submit-answer-inner');
+    bg.stop(true, true);
     outer.stop(true, true);
     if (outer.is(':visible')) {
+	bg.fadeTo('slow', 0, function() {
+	    bg.hide();
+        });
         outer.fadeToggle('slow', function() {
             inner.html('');
         });
     } else {
         inner.html('<iframe src="{{ root }}/dynamic/submit/meta/{{ round.round.url }}/" id="mh-submit-answer-iframe"></iframe>');
+	bg.fadeTo('slow', 0.5);
         outer.fadeToggle('slow');
     }
 };
+$('#mh-submit-answer-bg').click(mh.submit_answer);
